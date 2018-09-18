7b5feb66024fea3be8d2e651442cf87175789460
Denis Davydov <den.saint.k@gmail.com>
2018-09-18 11:13:27 +0300

    add menu and page with services

--- build/assets/scripts/main.js
+++ build/assets/scripts/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+    $('.header__menu').on('click', function() {
+        $('.menu').addClass('menu--active');
+        $('.wrapper').addClass('wrapper--active');
+        $('.wrapper__bg').addClass('wrapper__bg--active');
+    });
 
+    $('.menu__exit, .wrapper__bg').on('click', function() {
+        $('.menu').removeClass('menu--active');
+        $('.wrapper').removeClass('wrapper--active');
+        $('.wrapper__bg').removeClass('wrapper__bg--active');
+    });
 });
 
